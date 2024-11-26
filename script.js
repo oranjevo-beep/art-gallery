bb2fd95562175687197a2856ecf91d70e8dc8d10
Denys <oranjevo@gmail.com>
2024-11-26 16:13:18 +0800

    hamburger button added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 document.addEventListener('DOMContentLoaded', function () {
   const items = document.querySelectorAll('.group.item');
 
@@ -11,3 +13,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+const navToggle = () => {
+  menuBtn.classList.toggle('open');
+  menu.classList.toggle('hidden');
+  menu.classList.toggle('flex');
+};
+menuBtn.addEventListener('click', navToggle);
